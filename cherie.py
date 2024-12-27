0530087ceabf86c775e4b0a835d1b27b1b6b74b0
aimeshabani <95954431+aimeshabani@users.noreply.github.com>
2024-12-27 15:11:15 +0300

    Update cherie.py

--- cherie.py
+++ cherie.py
@@ -1,13 +1,1 @@
-def biefait(jina):
-    t=input(jina+' nipe ripoti yako:')
-    return t
 
-
-R={'AIME':11,'SIFA':20,'MUSUBAO':15,'JULLY':5}
-S=R.get('AIME',0)                                      #'MUTU HUYU HAYALETA RIPOTI'
-
-if S :
-    print('aime alileta ripoti ya saa ',S)
-else:
-    print('aime hakuleta ripoti')
-    print("sasa aime ameripoti saa ",biefait('aime'))
